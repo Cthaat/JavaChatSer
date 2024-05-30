180b067596944344aeffe4a12c96c47f758fc9a4
kc++ <2745296803@qq.com>
2024-05-30 10:16:19 +0800

    测试服务器连接

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,33 @@
 package org.example;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        try (ServerSocket serverSocket = new ServerSocket(8888) ;
+             Socket socket = serverSocket.accept() ;
+             InputStream is = socket.getInputStream();
+             DataInputStream dis = new DataInputStream(is))
+        {
+            while (true)
+            {
+                String message = dis.readUTF();
+                System.out.println("Received message: " + message);
+                if (message.equals("exit"))
+                {
+                    break;
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("Quit: " + e.getMessage());
+        }
     }
 }
