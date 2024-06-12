aba256a4998381578df302a9727e8e589b692571
kc++ <2745296803@qq.com>
2024-06-12 18:52:51 +0800

    Http测试成功

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,33 @@
 package org.example;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Collectors;
 
-public class Main
+@WebServlet ("/hello")
+public class Main extends HttpServlet
 {
-    public static void main(String[] args)
+    @Override
+    protected void doGet(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException
     {
-        try (ServerSocket serverSocket = new ServerSocket(8888);)
-        {
-            while (true)
-            {
-                Socket socket = serverSocket.accept();
-                new serverThread(socket).start();
-            }
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
+        this.doPost(req , resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException
+    {
+        String jsonString = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        System.out.println(jsonString);
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setContentType("application/json");
+        resp.getWriter().write("{\"status\":\"success\"}");
     }
 }
